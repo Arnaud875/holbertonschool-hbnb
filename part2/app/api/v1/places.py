b4fa4338a3c3a80dc77ad398018f7f14cc32de1c
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-10-21 09:49:13 +0000

    adding review model to places.py

---
+++ part2/app/api/v1/places.py
@@ -0,0 +1,7 @@
+# Adding the review model
+review_model = api.model('PlaceReview', {
+    'id': fields.String(description='Review ID'),
+    'text': fields.String(description='Text of the review'),
+    'rating': fields.Integer(description='Rating of the place (1-5)'),
+    'user_id': fields.String(description='ID of the user')
+})
