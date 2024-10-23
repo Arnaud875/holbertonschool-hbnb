ce4ae0818aa04edd7e2078a2ef9195492f648f0a
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-10-23 14:22:12 +0000

    updating review files

--- part2/app/models/review.py
+++ part2/app/models/review.py
@@ -1,12 +1,12 @@
 from .base import BaseModel
 
 class Review(BaseModel):
-    def __init__(self, text, rating, place, user):
+    def __init__(self, text, rating, place_id, user_id):
         super().__init__()
         if not (1 <= rating <= 5):
             raise ValueError("Rating must be between 1 and 5")
 
         self.text = text
         self.rating = rating
-        self.place = place
-        self.user = user
+        self.place_id = place_id
+        self.user_id = user_id
