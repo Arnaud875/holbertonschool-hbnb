c5b8b867b364a07571cf1e3f78249d8c65f12d3a
Ashe2A <supersonicx001@gmail.com>
2024-10-25 15:45:10 +0200

    Implement Places API

--- part2/app/api/v1/places.py
+++ part2/app/api/v1/places.py
@@ -1,7 +1,120 @@
-# Adding the review model
-# review_model = api.model('PlaceReview', {
-#     'id': fields.String(description='Review ID'),
-#     'text': fields.String(description='Text of the review'),
-#     'rating': fields.Integer(description='Rating of the place (1-5)'),
-#     'user_id': fields.String(description='ID of the user')
-# })
+#!/usr/bin/python3
+
+"""
+Places' API.
+"""
+
+
+from flask import Flask
+from flask_restx import fields, Namespace, Resource
+from app.services.facade import HBnBFacade
+
+
+api = Namespace("places", description="Place operations")
+app = Flask(__name__)
+facade = HBnBFacade.get_instance()
+place_model = api.model("Place", {
+    "title": fields.String(required=True, description="Place title"),
+    "description": fields.String(required=True, description="Place desc."),
+    "price": fields.String(required=True, description="Place's rent price"),
+    "latitude": fields.String(required=True, description="Place latitude"),
+    "longitude": fields.String(required=True, description="Place longitude"),
+    "owner": fields.String(required=True, description="Place owner")
+    "reviews": fields.String(required=False, description="Place's reviews"),
+    "amenities": fields.String(required=False, description="Place's amenity")
+})
+
+
+@api.route("/")
+class PlaceList(Resource):
+
+    """
+    Handle the place database
+    """
+
+    @api.expect(place_model, validate=True)
+    @api.response(201, "Place successfully created")
+    @api.response(400, "Invalid input data")
+    @api.response(404, "Owner not found")
+    def post(self):
+
+        """
+        POST a new place in database
+        """
+
+        place_data = api.payload
+
+        try:
+            new_place = facade.create_place(place_data)
+        except ValueError as e:
+            return {"error": str(e)}
+        
+        owner = facade.get_user(new_place.owner)
+        if not owner:
+            return {'error': "Owner not found"}, 404
+
+        return {
+            "id": new_place.id,
+            "title": new_place.title(),
+            "description": new_place.description,
+            "price": new_place.price(),
+            "latitude": new_place.latitude(),
+            "longitude": new_place.longitude(),
+            "owner": owner,
+            "reviews": new_place.reviews,
+            "amenities": new_place.amenities
+            }, 201
+
+
+@api.route("/<place_id>")
+class PlaceResource(Resource):
+
+    """
+    Display data of place
+    """
+
+    @api.response(200, "Place details retrieved successfully")
+    @api.response(404, "Place not found")
+    def get(self, place_id):
+        """
+        GET place details from their ID
+        """
+
+        place = facade.get_place(place_id)
+        if not place:
+            return {"error": "Place not found"}, 404
+        
+        owner = facade.get_user(place.owner)
+        if not owner:
+            return {'error': "Owner not found"}, 404
+
+        return {
+            "id": place.id,
+            "title": place.title,
+            "description": place.description,
+            "price": place.price,
+            "latitude": place.latitude(),
+            "longitude": place.longitude(),
+            "owner": owner,
+            "reviews": place.reviews,
+            "amenities": place.amenities
+            }, 200
+
+    @api.expect(place_model)
+    @api.response(200, "Place updated successfully")
+    @api.response(400, "Invalid input data")
+    def put(self, place_id):
+
+        """
+        PUT place details with their ID
+        """
+
+        obj = facade.get_place(place_id)
+        if not obj:
+            return {"error": "Place not found"}, 404
+
+        try:
+            facade.update_place(place_id, api.payload)
+        except Exception as e:
+            return {"error": "Invalid input data"}, 400
+        return {"message": "Place updated successfully"}, 200
--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -17,6 +17,7 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
+    # User facade
     def create_user(self, user_data):
         user = User(**user_data)
         self.user_repo.add(user)
@@ -31,6 +32,7 @@ class HBnBFacade:
     def update_user(self, user_id, user_data):
         return self.user_repo.update(user_id, user_data)
 
+    # Amenity facade
     def create_amenity(self, amenity_data):
         amenity = Amenity(**amenity_data)
         self.amenity_repo.add(amenity)
@@ -44,12 +46,34 @@ class HBnBFacade:
 
     def update_amenity(self, amenity_id, amenity_data):
         return self.amenity_repo.update(amenity_id, amenity_data)
+    
+    # Place facade
+    def create_place(self, place_data):
+        place = Place(**place_data)
+        self.place_repo.add(place)
+        return place
 
+    def get_place(self, place_id):
+        return self.place_repo.get(place_id)
+    
+    def get_all_places(self):
+        return self.place_repo.get_all()
+    
+    def get_place_by_user(self, user_id):
+        return self.place_repo.get_by_attribute("id", user_id)
+    
+    def update_place(self, place_id, place_data):
+        return self.place_repo.update(place_id, place_data)
+
+    def delete_place(self, place_id):
+        return self.place_repo.delete(place_id)
+
+    # Review facade
     def create_review(self, review_data):
         review = Review(**review_data)
         self.review_repo.add(review)
         return review
-
+    
     def get_review(self, review_id):
         return self.review_repo.get(review_id)
 
